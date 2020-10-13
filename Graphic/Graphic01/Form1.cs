29702e2c05b0b8ae0bb16a9cb86d38c911687499
bgw <giung609@naver.com>
2020-10-13 11:10:24 +0900

    2020-10 smt

---
+++ Graphic/Graphic01/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Graphic01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Graphics graphics = CreateGraphics();
+            Show();
+
+            Pen BluePen = new Pen(Color.Blue, 3);
+            graphics.DrawLine(BluePen, 20, 70, 30, 80);
+            graphics.DrawLine(BluePen, 30, 80, 40, 70);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
